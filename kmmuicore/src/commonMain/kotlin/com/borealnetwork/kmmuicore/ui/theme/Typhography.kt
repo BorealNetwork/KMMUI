ebda4bd3e10a47b8d687b4e2425d3014e8bd73be
Baudick <baudelio_andalon@hotmail.com>
2025-12-07 12:49:39 -0600

    [FEATURE] Baudelio
    * New function in viewModel

--- kmmuicore/src/commonMain/kotlin/com/borealnetwork/kmmuicore/ui/theme/Typhography.kt
+++ kmmuicore/src/commonMain/kotlin/com/borealnetwork/kmmuicore/ui/theme/Typhography.kt
@@ -5,6 +5,10 @@ import org.jetbrains.compose.resources.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import io.github.baudelioandalon.kmmuicore.drawable.Res
+import io.github.baudelioandalon.kmmuicore.drawable.montserrat_bold
+import io.github.baudelioandalon.kmmuicore.drawable.montserrat_regular
+import io.github.baudelioandalon.kmmuicore.drawable.montserrat_semibold
+import io.github.baudelioandalon.kmmuicore.drawable.montserrat_thin
 import io.github.baudelioandalon.kmmuicore.drawable.roboto
 import io.github.baudelioandalon.kmmuicore.drawable.roboto_bold
 import io.github.baudelioandalon.kmmuicore.drawable.roboto_light
@@ -16,6 +20,13 @@ fun robotoFamily() = FontFamily(
     Font(Res.font.roboto, FontWeight.SemiBold), // Puedes usar Medium o Bold si no tienes un archivo SemiBold
     Font(Res.font.roboto_bold, FontWeight.Bold)
 )
+@Composable
+fun montserratFamily() = FontFamily(
+    Font(Res.font.montserrat_thin, FontWeight.Light),
+    Font(Res.font.montserrat_regular, FontWeight.Normal),
+    Font(Res.font.montserrat_semibold, FontWeight.SemiBold), // Puedes usar Medium o Bold si no tienes un archivo SemiBold
+    Font(Res.font.montserrat_bold, FontWeight.Bold)
+)
 
 @Composable
-fun appTypography() = robotoFamily()
+fun appTypography(defaultFontFamily: FontFamily = robotoFamily()) = defaultFontFamily
