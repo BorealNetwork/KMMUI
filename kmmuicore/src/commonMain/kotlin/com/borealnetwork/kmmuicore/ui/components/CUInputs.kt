085e56290edf8b2d41b5097d2ee19da53a433ddb
Baudick <baudelio_andalon@hotmail.com>
2025-12-07 13:54:10 -0600

    [FEATURE] Baudelio
    * New function in viewModel

--- kmmuicore/src/commonMain/kotlin/com/borealnetwork/kmmuicore/ui/components/CUInputs.kt
+++ kmmuicore/src/commonMain/kotlin/com/borealnetwork/kmmuicore/ui/components/CUInputs.kt
@@ -1,10 +1,14 @@
 package com.borealnetwork.kmmuicore.ui.components
 
 import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.interaction.MutableInteractionSource
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.KeyboardActions
 import androidx.compose.foundation.text.KeyboardOptions
@@ -12,6 +16,7 @@ import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.OutlinedTextFieldDefaults
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextFieldColors
 import androidx.compose.material3.TextFieldDefaults
@@ -23,9 +28,13 @@ import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.Color.Companion.Black
+import androidx.compose.ui.graphics.Color.Companion.White
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.font.FontWeight.Companion.Bold
 import androidx.compose.ui.text.font.FontWeight.Companion.Medium
 import androidx.compose.ui.text.font.FontWeight.Companion.SemiBold
 import androidx.compose.ui.text.input.KeyboardCapitalization
@@ -37,15 +46,23 @@ import androidx.compose.ui.unit.TextUnit
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.borealnetwork.kmmuicore.domain.base.EMPTY_STRING
+import com.borealnetwork.kmmuicore.ui.theme.BorderDisabledColor
 import com.borealnetwork.kmmuicore.ui.theme.CUGrayColor
 import com.borealnetwork.kmmuicore.ui.theme.CUGrayLetterHint
 import com.borealnetwork.kmmuicore.ui.theme.GrayLetterHint
+import com.borealnetwork.kmmuicore.ui.theme.PrimaryColor
+import com.borealnetwork.kmmuicore.ui.theme.TextDisabledColor
+import com.borealnetwork.kmmuicore.ui.theme.TextTopColor
 import com.borealnetwork.kmmuicore.ui.theme.appTypography
+import com.borealnetwork.kmmuicore.ui.theme.robotoFamily
 import com.borealnetwork.kmmuicore.utils.containsAlphanumericCharacters
 import com.borealnetwork.kmmuicore.utils.isDigitsOnly
 import io.github.baudelioandalon.kmmuicore.drawable.Res
 import io.github.baudelioandalon.kmmuicore.drawable.ic_design_invisibility
 import io.github.baudelioandalon.kmmuicore.drawable.ic_design_visibility
+import io.github.baudelioandalon.kmmuicore.drawable.ic_help
+import io.github.baudelioandalon.kmmuicore.drawable.ic_qrcode
+import org.jetbrains.compose.resources.DrawableResource
 import org.jetbrains.compose.resources.painterResource
 import org.jetbrains.compose.ui.tooling.preview.Preview
 
@@ -177,56 +194,74 @@ fun EditText(
 @Composable
 fun EditTextTopLabel(
     modifier: Modifier = Modifier,
-    placeHolderText: String = "",
-    onValueChange: (TextFieldValue) -> Unit,
-    isError: Boolean = false,
     value: TextFieldValue,
-    errorMessage: String = "",
     keyboardOptions: KeyboardOptions = KeyboardOptions(
         capitalization = KeyboardCapitalization.Words,
         keyboardType = KeyboardType.Text
     ),
     keyboardActions: KeyboardActions = KeyboardActions { },
-    trailingIcon: @Composable (() -> Unit)? = null,
-    enabled: Boolean = true,
-    topLabelText: String = "",
-    enabledHelper: Boolean = false,
-    enabledCounter: Boolean = false,
+    singleLine: Boolean = true,
+    topLabelText: String = EMPTY_STRING,
+    placeHolderText: String = EMPTY_STRING,
     maxLength: Int = 0,
+    enabled: Boolean = false,
+    enabledCounter: Boolean = false,
     textSize: TextUnit = 15.sp,
-    singleLine: Boolean = false,
-    colors: TextFieldColors = TextFieldDefaults.colors(unfocusedLabelColor = GrayLetterHint),
-    isPassword: Boolean = false,
-    visualTransformation: VisualTransformation = VisualTransformation.None
+    textTopSize: TextUnit = 14.sp,
+    iconTint: Color = PrimaryColor,
+    borderColor: Color = PrimaryColor,
+    textColor: Color = Black,
+    textTopColor: Color = TextTopColor,
+    textDisabledColor: Color = TextDisabledColor,
+    borderDisabledColor: Color = BorderDisabledColor,
+    endIcon: DrawableResource = Res.drawable.ic_qrcode,
+    showTopEndIcon: Boolean = true,
+    topEndIcon: DrawableResource = Res.drawable.ic_help,
+    fontFamilyTop: FontFamily = robotoFamily(),
+    fontWeightTop: FontWeight = SemiBold,
+    fontFamilyPlaceholder: FontFamily = robotoFamily(),
+    fontWeightPlaceholder: FontWeight = Bold,
+    endIconClicked: () -> Unit = {},
+    topEndIconClicked: () -> Unit = {},
+    onValueChange: (TextFieldValue) -> Unit
 ) {
     val interactionSource = remember { MutableInteractionSource() }
-    var passwordVisibility by remember { mutableStateOf(isPassword) }
-    val icon = if (passwordVisibility)
-        painterResource(resource = Res.drawable.ic_design_invisibility)
-    else
-        painterResource(resource = Res.drawable.ic_design_visibility)
 
-    Column(modifier = modifier) {
+    val backgroundBorderColor = if (enabled) borderColor else borderDisabledColor
+    val textFieldColor = if (enabled) textColor else textDisabledColor
 
-        Text(
-            modifier = Modifier.padding(bottom = 9.dp),
-            text = topLabelText,
-            color = Black,
-            fontSize = textSize,
-            fontWeight = Medium,
-            fontFamily = appTypography()
-        )
-
-        OutlinedTextField(
+    Column(
+        modifier = modifier.fillMaxWidth()
+    ) {
+        // 1. Fila Superior: Etiqueta "Token" e Icono de Ayuda (?)
+        Row(
             modifier = Modifier
-                .fillMaxWidth(),
-            textStyle = TextStyle(
-                fontFamily = appTypography(),
-                color = Black,
-                fontWeight = SemiBold,
-                fontSize = textSize
-            ),
-            interactionSource = interactionSource,
+                .fillMaxWidth()
+                .padding(bottom = 4.dp, top = 40.dp), // Espacio entre etiqueta y caja
+            horizontalArrangement = Arrangement.SpaceBetween,
+        ) {
+            Text(
+                text = topLabelText,
+                fontSize = textTopSize,
+                color = textTopColor,
+                fontFamily = fontFamilyTop,
+                fontWeight = fontWeightTop
+            )
+            if (showTopEndIcon) {
+                Icon(
+                    painter = painterResource(topEndIcon),
+                    contentDescription = "Ayuda",
+                    tint = iconTint,
+                    modifier = Modifier
+                        .size(20.dp)
+                        .clickable { topEndIconClicked() }
+                )
+            }
+
+        }
+
+        // 2. Campo de Texto (OutlinedTextField)
+        OutlinedTextField(
             value = value,
             onValueChange = {
                 if (enabledCounter) {
@@ -236,44 +271,40 @@ fun EditTextTopLabel(
                 } else {
                     onValueChange(it)
                 }
-            }, placeholder = {
-                SemiBoldText(
+            },
+            interactionSource = interactionSource,
+            modifier = Modifier.fillMaxWidth(),
+            placeholder = {
+                Text(
                     text = placeHolderText,
-                    color = GrayLetterHint,
-                    fontSize = 15.sp
+                    fontSize = textSize,
+                    color = textFieldColor,
+                    fontFamily = fontFamilyPlaceholder,
+                    fontWeight = fontWeightPlaceholder
                 )
             },
-            colors = colors,
-            isError = isError,
-            keyboardOptions = if (isPassword) KeyboardOptions(
-                keyboardType = KeyboardType.Password
-            ) else keyboardOptions,
+            keyboardOptions = keyboardOptions,
             trailingIcon = {
-                if (isPassword) IconButton(onClick = {
-                    passwordVisibility = !passwordVisibility
-                }) {
+                IconButton(onClick = endIconClicked) {
                     Icon(
-                        painter = icon,
-                        contentDescription = "Visibility Icon"
+                        painter = painterResource(endIcon),
+                        contentDescription = "Escanear QR",
+                        tint = iconTint
                     )
-                } else trailingIcon
+                }
             },
             enabled = enabled,
             singleLine = singleLine,
             keyboardActions = keyboardActions,
-            visualTransformation = if (!passwordVisibility) visualTransformation
-            else PasswordVisualTransformation(mask = '●')
-        )
-
-        AnimatedVisibility(visible = isError && errorMessage.isNotEmpty()) {
-            Text(
-                text = errorMessage,
-                color = MaterialTheme.colorScheme.error,
-                fontSize = 15.sp,
-                fontWeight = Medium,
-                fontFamily = appTypography()
+            shape = RoundedCornerShape(2.dp),
+            colors = OutlinedTextFieldDefaults.colors(
+                focusedBorderColor = backgroundBorderColor,
+                unfocusedBorderColor = backgroundBorderColor,
+                cursorColor = backgroundBorderColor,
+                focusedContainerColor = White,
+                unfocusedContainerColor = White
             )
-        }
+        )
     }
 }
 
