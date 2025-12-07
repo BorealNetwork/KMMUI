62881c5039e30f17cbf92875cd700494a7abeb15
Baudick <baudelio_andalon@hotmail.com>
2025-12-07 13:16:10 -0600

    [FEATURE] Baudelio
    * New function in viewModel

--- kmmuicore/src/commonMain/kotlin/com/borealnetwork/kmmuicore/ui/theme/Typhography.kt
+++ kmmuicore/src/commonMain/kotlin/com/borealnetwork/kmmuicore/ui/theme/Typhography.kt
@@ -1,30 +1,40 @@
 package com.borealnetwork.kmmuicore.ui.theme
 
 import androidx.compose.runtime.Composable
-import org.jetbrains.compose.resources.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import io.github.baudelioandalon.kmmuicore.drawable.Res
 import io.github.baudelioandalon.kmmuicore.drawable.montserrat_bold
+import io.github.baudelioandalon.kmmuicore.drawable.montserrat_light
+import io.github.baudelioandalon.kmmuicore.drawable.montserrat_medium
 import io.github.baudelioandalon.kmmuicore.drawable.montserrat_regular
 import io.github.baudelioandalon.kmmuicore.drawable.montserrat_semibold
 import io.github.baudelioandalon.kmmuicore.drawable.montserrat_thin
-import io.github.baudelioandalon.kmmuicore.drawable.roboto
 import io.github.baudelioandalon.kmmuicore.drawable.roboto_bold
 import io.github.baudelioandalon.kmmuicore.drawable.roboto_light
+import io.github.baudelioandalon.kmmuicore.drawable.roboto_medium
+import io.github.baudelioandalon.kmmuicore.drawable.roboto_regular
+import io.github.baudelioandalon.kmmuicore.drawable.roboto_semibold
+import io.github.baudelioandalon.kmmuicore.drawable.roboto_thin
+import org.jetbrains.compose.resources.Font
 
 @Composable
 fun robotoFamily() = FontFamily(
+    Font(Res.font.roboto_thin, FontWeight.Thin),
     Font(Res.font.roboto_light, FontWeight.Light),
-    Font(Res.font.roboto, FontWeight.Normal),
-    Font(Res.font.roboto, FontWeight.SemiBold), // Puedes usar Medium o Bold si no tienes un archivo SemiBold
+    Font(Res.font.roboto_regular, FontWeight.Normal),
+    Font(Res.font.roboto_medium, FontWeight.Medium),
+    Font(Res.font.roboto_semibold, FontWeight.SemiBold),
     Font(Res.font.roboto_bold, FontWeight.Bold)
 )
+
 @Composable
 fun montserratFamily() = FontFamily(
-    Font(Res.font.montserrat_thin, FontWeight.Light),
+    Font(Res.font.montserrat_thin, FontWeight.Thin),
+    Font(Res.font.montserrat_light, FontWeight.Light),
     Font(Res.font.montserrat_regular, FontWeight.Normal),
-    Font(Res.font.montserrat_semibold, FontWeight.SemiBold), // Puedes usar Medium o Bold si no tienes un archivo SemiBold
+    Font(Res.font.montserrat_medium, FontWeight.Medium),
+    Font(Res.font.montserrat_semibold, FontWeight.SemiBold),
     Font(Res.font.montserrat_bold, FontWeight.Bold)
 )
 
