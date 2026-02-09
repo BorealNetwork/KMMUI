69f4a65751ae2a7860166ba356aa9f89f62d1bec
Baudick <baudelio_andalon@hotmail.com>
2026-02-08 20:18:36 -0600

    [FEATURE] Baudelio
    * New SecondaryButtonImg

--- kmmuicore/src/commonMain/kotlin/com/borealnetwork/kmmuicore/ui/components/CUButtons.kt
+++ kmmuicore/src/commonMain/kotlin/com/borealnetwork/kmmuicore/ui/components/CUButtons.kt
@@ -5,10 +5,13 @@ import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
@@ -21,6 +24,9 @@ import androidx.compose.ui.unit.TextUnit
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.borealnetwork.kmmuicore.ui.theme.CUDisabledColor
+import com.borealnetwork.kmmuicore.ui.theme.PrimaryColor
+import org.jetbrains.compose.resources.DrawableResource
+import org.jetbrains.compose.resources.painterResource
 import org.jetbrains.compose.ui.tooling.preview.Preview
 
 
@@ -128,6 +134,70 @@ fun SecondaryButton(
             color = textColor,
             fontSize = fontSize
         )
+    }
+
+}
+
+@Composable
+fun SecondaryButtonImg(
+    modifier: Modifier = Modifier,
+    text: String = "text",
+    borderRadius: Dp = 8.dp,
+    backgroundEnabledColor: Color = Color.Transparent,
+    backgroundDisabledColor: Color = CUDisabledColor,
+    textEnabledColor: Color = MaterialTheme.colorScheme.primary,
+    textDisabledColor: Color = DarkGray,
+    borderEnabledColor: Color = MaterialTheme.colorScheme.primary,
+    borderDisabledColor: Color = CUDisabledColor,
+    fontSize: TextUnit = 13.sp,
+    height: Dp = 48.dp,
+    enabled: Boolean = true,
+    topEndIcon: DrawableResource? = null,
+    topEndIconClicked: () -> Unit = {},
+    onClick: () -> Unit = {}
+) {
+    val backgroundColor = if (enabled) backgroundEnabledColor else backgroundDisabledColor
+    val textColor = if (enabled) textEnabledColor else textDisabledColor
+    val borderColor =
+        if (enabled) borderEnabledColor else borderDisabledColor
+    Box(
+        modifier = modifier
+            .fillMaxWidth()
+            .height(height)
+            .border(
+                width = 2.dp,
+                color = borderColor,      // Azul similar
+                shape = RoundedCornerShape(borderRadius) // Esquinas redondeadas
+            )
+            .background(
+                color = backgroundColor,        // Fondo gris muy claro
+                shape = RoundedCornerShape(borderRadius)
+            )
+            .clickable(enabled = enabled, onClick = onClick),
+        contentAlignment = Alignment.Center
+    ) {
+        Row(
+            verticalAlignment = Alignment.CenterVertically
+        ){
+            BoldText(
+                text = text,
+                color = textColor,
+                fontSize = fontSize
+            )
+            if (topEndIcon != null) {
+                Icon(
+                    painter = painterResource(topEndIcon),
+                    contentDescription = "Ayuda",
+                    tint = PrimaryColor,
+                    modifier = Modifier
+                        .size(20.dp).padding(start = 5.dp)
+                        .clickable(onClick = topEndIconClicked)
+                )
+            }
+        }
+
+
+
     }
 
 }
