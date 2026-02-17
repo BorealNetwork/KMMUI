8ebc2925391ecc5b0e6a12f949e81ea1adb02b1e
Baudelio Andalon <baudelio.andalon@grupobimbo.com>
2026-02-17 12:26:25 -0600

    [FEATURE] NEW FONT
    *ONEST

--- kmmuicore/src/commonMain/kotlin/com/borealnetwork/kmmuicore/ui/theme/Typhography.kt
+++ kmmuicore/src/commonMain/kotlin/com/borealnetwork/kmmuicore/ui/theme/Typhography.kt
@@ -10,12 +10,18 @@ import io.github.baudelioandalon.kmmuicore.drawable.montserrat_medium
 import io.github.baudelioandalon.kmmuicore.drawable.montserrat_regular
 import io.github.baudelioandalon.kmmuicore.drawable.montserrat_semibold
 import io.github.baudelioandalon.kmmuicore.drawable.montserrat_thin
+import io.github.baudelioandalon.kmmuicore.drawable.onest_bold
+import io.github.baudelioandalon.kmmuicore.drawable.onest_light
+import io.github.baudelioandalon.kmmuicore.drawable.onest_medium
+import io.github.baudelioandalon.kmmuicore.drawable.onest_regular
+import io.github.baudelioandalon.kmmuicore.drawable.onest_semibold
 import io.github.baudelioandalon.kmmuicore.drawable.roboto_bold
 import io.github.baudelioandalon.kmmuicore.drawable.roboto_light
 import io.github.baudelioandalon.kmmuicore.drawable.roboto_medium
 import io.github.baudelioandalon.kmmuicore.drawable.roboto_regular
 import io.github.baudelioandalon.kmmuicore.drawable.roboto_semibold
 import io.github.baudelioandalon.kmmuicore.drawable.roboto_thin
+import io.github.baudelioandalon.kmmuicore.drawable.onest_thin
 import org.jetbrains.compose.resources.Font
 
 @Composable
@@ -26,6 +32,17 @@ fun robotoFamily() = FontFamily(
     Font(Res.font.roboto_medium, FontWeight.Medium),
     Font(Res.font.roboto_semibold, FontWeight.SemiBold),
     Font(Res.font.roboto_bold, FontWeight.Bold)
+)
+
+
+@Composable
+fun onestFamily() = FontFamily(
+    Font(Res.font.onest_thin, FontWeight.Thin),
+    Font(Res.font.onest_light, FontWeight.Light),
+    Font(Res.font.onest_regular, FontWeight.Normal),
+    Font(Res.font.onest_medium, FontWeight.Medium),
+    Font(Res.font.onest_semibold, FontWeight.SemiBold),
+    Font(Res.font.onest_bold, FontWeight.Bold)
 )
 
 @Composable
